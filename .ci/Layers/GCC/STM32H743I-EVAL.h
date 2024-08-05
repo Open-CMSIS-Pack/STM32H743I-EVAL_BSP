628a4cdf891a9fd31b0f9a23e96c00188f2cec40
DavidLesnjak <david.lesnjak@arm.com>
2024-08-05 14:26:28 +0200

    Add GCC Board Layer to be used by CI

---
+++ .ci/Layers/GCC/STM32H743I-EVAL.h
@@ -0,0 +1,52 @@
+/*---------------------------------------------------------------------------
+ * Copyright (c) 2024 Arm Limited (or its affiliates).
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *---------------------------------------------------------------------------*/
+
+#ifndef STM32H743I_EVAL_H_
+#define STM32H743I_EVAL_H_
+
+#include "GPIO_STM32.h"
+#include "Driver_ETH_MAC.h"
+#include "Driver_ETH_PHY.h"
+#include "Driver_MCI.h"
+#include "Driver_USART.h"
+#include "Driver_USBD.h"
+#include "Driver_USBH.h"
+
+// CMSIS Driver instances of Board peripherals
+#define CMSIS_DRIVER_ETH    0   // CMSIS-Driver ETH instance number
+#define CMSIS_DRIVER_MCI    1   // CMSIS-Driver MCI instance number
+#define CMSIS_DRIVER_USBD   0   // CMSIS-Driver USB Device instance number
+#define CMSIS_DRIVER_USBH   1   // CMSIS-Driver USB Host instance number
+
+// Retarget stdio to CMSIS UART
+#define RETARGET_STDIO_UART 1
+
+// CMSIS Drivers
+extern ARM_DRIVER_ETH_MAC ARM_Driver_ETH_MAC_(CMSIS_DRIVER_ETH);  // ETH MAC
+extern ARM_DRIVER_ETH_PHY ARM_Driver_ETH_PHY_(CMSIS_DRIVER_ETH);  // ETH PHY
+extern ARM_DRIVER_MCI     ARM_Driver_MCI_(CMSIS_DRIVER_MCI);      // MCI
+extern ARM_DRIVER_USART   ARM_Driver_USART_(RETARGET_STDIO_UART); // ST-Link
+extern ARM_DRIVER_USBD    ARM_Driver_USBD_(CMSIS_DRIVER_USBD);    // USB Device
+extern ARM_DRIVER_USBH    ARM_Driver_USBH_(CMSIS_DRIVER_USBH);    // USB Host
+
+#ifdef CMSIS_shield_header
+#include CMSIS_shield_header
+#endif
+
+#endif /* STM32H743I_EVAL_H_ */
