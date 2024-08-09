906ab11675b171185147b4a395b4139f4f423640
DavidLesnjak <david.lesnjak@arm.com>
2024-08-09 07:19:51 +0200

    Update GCC Board layer (add memory regions for ETH and MCI driver)
    * Note: Linker script code generated by STM32CubeMX has been customized. Regenerating the code will overwrite these manual adjustments.

--- .ci/Layers/GCC/CubeMX/MX_Device/MX_Device.h
+++ .ci/Layers/GCC/CubeMX/MX_Device/MX_Device.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File Name   : MX_Device.h
- * Date        : 05/08/2024 13:48:58
+ * Date        : 09/08/2024 07:25:40
  * Description : STM32Cube MX parameter definitions
  * Note        : This file is generated with a generator out of the
  *               STM32CubeMX project and its generated files (DO NOT EDIT!)
