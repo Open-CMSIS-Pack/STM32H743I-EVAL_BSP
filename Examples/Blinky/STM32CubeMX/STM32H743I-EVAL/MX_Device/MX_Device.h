ce3ccfa26298596fcebbd5590b5d03ffdef2ffe1
DavidLesnjak <david.lesnjak@arm.com>
2024-09-17 15:04:45 +0200

    Update Blinky example
    - Update MX_Device.h
    - Remove cdefault.yml
    - Update regions_STM32H743I-EVAL.h
    - Remove "# compiler: AC6" from Blinky.csolution.yml

--- Examples/Blinky/STM32CubeMX/STM32H743I-EVAL/MX_Device/MX_Device.h
+++ Examples/Blinky/STM32CubeMX/STM32H743I-EVAL/MX_Device/MX_Device.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File Name   : MX_Device.h
- * Date        : 06/09/2024 14:10:11
+ * Date        : 17/09/2024 13:57:33
  * Description : STM32Cube MX parameter definitions
  * Note        : This file is generated with a generator out of the
  *               STM32CubeMX project and its generated files (DO NOT EDIT!)
