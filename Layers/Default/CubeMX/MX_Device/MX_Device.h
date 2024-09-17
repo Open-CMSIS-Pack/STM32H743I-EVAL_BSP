50325ce89e8ad7d120b5fd002b3e2d65e132385f
DavidLesnjak <david.lesnjak@arm.com>
2024-09-17 15:05:00 +0200

    Update Default Board layer
    - Update MX_Device.h
    - Update regions_STM32H743I-EVAL.h and ac6_linker_script.sct.src

--- Layers/Default/CubeMX/MX_Device/MX_Device.h
+++ Layers/Default/CubeMX/MX_Device/MX_Device.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File Name   : MX_Device.h
- * Date        : 06/09/2024 14:36:37
+ * Date        : 18/09/2024 08:15:22
  * Description : STM32Cube MX parameter definitions
  * Note        : This file is generated with a generator out of the
  *               STM32CubeMX project and its generated files (DO NOT EDIT!)
@@ -134,6 +134,9 @@
 
 /* Mode */
 #define MX_SDMMC1_MODE_SD                       1
+
+/* Peripheral Clock Frequency */
+#define MX_SDMMC1_PERIPH_CLOCK_FREQ             75000000
 
 /* Pins */
 
