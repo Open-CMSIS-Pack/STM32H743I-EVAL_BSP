22eb94909ff0109068171d386d416f09989c9181
Ziga Mahne <ziga.mahne@arm.com>
2025-06-04 07:21:13 +0200

    Update Blinky example
    - specify thread names using thread attributes
    - rename thread IDs
    - modify app_main_thread (replace loop forever)
    - add DWARF-5 debug information
    - add RTE_Components.h files

--- Examples/Blinky/Blinky.c
+++ Examples/Blinky/Blinky.c
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------
- * Copyright (c) 2024 Arm Limited (or its affiliates).
+ * Copyright (c) 2024-2025 Arm Limited (or its affiliates).
  * All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -18,19 +18,29 @@
  *---------------------------------------------------------------------------*/
 
 #include <stdio.h>
-
 #include "main.h"
-
-#include "cmsis_os2.h"                  // ::CMSIS:RTOS2
+#include "cmsis_os2.h"
 #include "cmsis_vio.h"
 
-static osThreadId_t tid_thrLED;         // Thread id of thread: LED
-static osThreadId_t tid_thrButton;      // Thread id of thread: Button
+/* Thread attributes for the app_main thread */
+static const osThreadAttr_t thread_attr_main = { .name = "app_main" };
 
-/*-----------------------------------------------------------------------------
-  thrLED: blink LED
- *----------------------------------------------------------------------------*/
-static __NO_RETURN void thrLED (void *argument) {
+/* Thread attributes for the LED thread */
+static const osThreadAttr_t thread_attr_LED = { .name = "LED" };
+
+/* Thread attributes for the Button thread */
+static const osThreadAttr_t thread_attr_Button = { .name = "Button" };
+
+/* Thread ID for the LED thread */
+static osThreadId_t tid_LED;
+
+/* Thread ID for the Button thread */
+static osThreadId_t tid_Button;
+
+/*
+  Thread that blinks LED.
+*/
+static __NO_RETURN void thread_LED (void *argument) {
   uint32_t active_flag = 0U;
 
   (void)argument;
@@ -57,20 +67,20 @@ static __NO_RETURN void thrLED (void *argument) {
   }
 }
 
-/*-----------------------------------------------------------------------------
-  thrButton: check Button state
- *----------------------------------------------------------------------------*/
-static __NO_RETURN void thrButton (void *argument) {
+/*
+  Thread that checks Button state.
+*/
+static __NO_RETURN void thread_Button (void *argument) {
   uint32_t last = 0U;
   uint32_t state;
 
   (void)argument;
 
   for (;;) {
-    state = (vioGetSignal(vioBUTTON0));           // Get pressed Button state
+    state = (vioGetSignal(vioBUTTON0));         // Get pressed Button state
     if (state != last) {
       if (state == 1U) {
-        osThreadFlagsSet(tid_thrLED, 1U);         // Set flag to thrLED
+        osThreadFlagsSet(tid_LED, 1U);          // Set flag to thread_LED
       }
       last = state;
     }
@@ -78,26 +88,30 @@ static __NO_RETURN void thrButton (void *argument) {
   }
 }
 
-/*-----------------------------------------------------------------------------
- * Application main thread
- *----------------------------------------------------------------------------*/
+/*
+  Application main thread.
+*/
 __NO_RETURN void app_main_thread (void *argument) {
+  (void)argument;
 
   printf("Blinky example\n");
 
-  tid_thrLED = osThreadNew(thrLED, NULL, NULL);         // Create LED thread
-  tid_thrButton = osThreadNew(thrButton, NULL, NULL);   // Create Button thread
+  /* Create LED and Button threads */
+  tid_LED = osThreadNew(thread_LED, NULL, &thread_attr_LED);
+  tid_Button = osThreadNew(thread_Button, NULL, &thread_attr_Button);
 
-  for (;;) {                            // Loop forever
+  for (;;) {
+    /* Delay indefinitely */
+    osDelay(osWaitForever);
   }
 }
 
-/*-----------------------------------------------------------------------------
- * Application initialization
- *----------------------------------------------------------------------------*/
+/*
+  Application initialization.
+*/
 int app_main (void) {
-  osKernelInitialize();                         /* Initialize CMSIS-RTOS2 */
-  osThreadNew(app_main_thread, NULL, NULL);
-  osKernelStart();                              /* Start thread execution */
+  osKernelInitialize();                         // Initialize CMSIS-RTOS2
+  osThreadNew(app_main_thread, NULL, &thread_attr_main);
+  osKernelStart();                              // Start thread execution
   return 0;
 }
